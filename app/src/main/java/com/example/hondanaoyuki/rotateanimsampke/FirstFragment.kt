3d552ea7db4c875e74df6584a9a6358a1141fa5c
本田尚行 <hondayuki0606@yahoo.co.jp>
2021-05-15 20:46:59 +0900

    Commit Fragment

---
+++ app/src/main/java/com/example/hondanaoyuki/rotateanimsampke/FirstFragment.kt
@@ -0,0 +1,21 @@
+package com.example.hondanaoyuki.rotateanimsampke
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FirstFragment : Fragment() {
+
+    // Fragmentで表示するViewを作成するメソッド
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        // 先ほどのレイアウトをここでViewとして作成します
+        return inflater.inflate(R.layout.fragment_barrier, container, false)
+    }
+}
--- app/src/main/java/com/example/hondanaoyuki/rotateanimsampke/MainActivity.kt
+++ app/src/main/java/com/example/hondanaoyuki/rotateanimsampke/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.hondanaoyuki.rotateanimsampke
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        // BackStackを設定
+        fragmentTransaction.addToBackStack(null)
+
+        // パラメータを設定
+        fragmentTransaction.replace(R.id.container, FirstFragment())
+        fragmentTransaction.commit()
     }
 }
